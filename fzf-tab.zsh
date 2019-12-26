63dd4d8011bb4501d29f9d0b80d09288a3b25550
Aloxaf <aloxafx@gmail.com>
2019-12-26 22:07:29 +0800

    Simplify the code

--- fzf-tab.zsh
+++ fzf-tab.zsh
@@ -1,16 +1,13 @@
-# for easier stripping
-zstyle ':completion:*' list-separator $'\0'
-
 zmodload zsh/zutil
 
 # thanks Valodim/zsh-capture-completion
 function compadd() {
     # parse all options
-    typeset -A apre hpre ipre hsuf asuf isuf arg_d arg_J arg_V \
+    typeset -A apre hpre ipre hsuf asuf isuf dscrs arg_J arg_V \
          arg_X arg_x arg_r arg_R arg_W arg_F arg_M arg_O arg_A arg_D arg_E
     local flag_a flag_k flag_l flag_o flag_1 flag_2 flag_q isfile \
          flag_e flag_Q flag_n flag_U flag_C
-    zparseopts -E P:=apre p:=hpre i:=ipre S:=asuf s:=hsuf I:=isuf d:=arg_d \
+    zparseopts -E P:=apre p:=hpre i:=ipre S:=asuf s:=hsuf I:=isuf d:=dscrs \
         J:=arg_J V:=arg_V X:=arg_X x:=arg_x r:=arg_r R:=arg_R W:=arg_W F:=arg_F \
         M:=arg_M O:=arg_O A:=arg_A D:=arg_D E:=arg_E \
         a=flag_a k=flag_k l=flag_l o=flag_o 1=flag_1 2=flag_2 q=flag_q \
@@ -24,8 +21,8 @@ function compadd() {
 
     # store matches in $__hits and descriptions in $__dscr
     typeset -a __hits __dscr
-    if (( $#arg_d == 1 )) {
-        __dscr=( "${(@P)${(v)arg_d}}" )
+    if (( $#dscrs == 1 )) {
+        __dscr=( "${(@P)${(v)dscrs}}" )
     }
     builtin compadd -A __hits -D __dscr "$@"
     if (( $#__hits == 0 )) {
@@ -43,18 +40,19 @@ function compadd() {
         }
     }
 
-    # add the matches and descriptions
-    setopt localoptions extendedglob
-    local dscr
+    # dscr - the string to show to users
+    # word - the string to be inserted
+    local dscr word
     for i ({1..$#__hits}) {
-        dscr=
-        if (( $#__dscr >= $i )) {
-            # TODO: show dscr to user directly?
-            dscr="${${${__dscr[$i]}##*$'\0' #}//$'\n'}"
+        word=$__hits[i] && dscr=$__dscr[i]
+        if [[ -n $dscr ]] {
+            dscr=${dscr//$'\n'}
+        } elif [[ -n $word ]] {
+            dscr=$word
+        } else {
+            continue
         }
-        if [[ -n $__hits[$i] ]] {
-            compcap_list[$__hits[$i]]=$__tmp_value${dscr:+$'\0'"dscr"$'\0'$dscr}
-        }
+        compcap_list[$dscr]=$__tmp_value${word:+$'\0'"word"$'\0'$word}
     }
 }
 
@@ -83,18 +81,15 @@ function _find_common_prefix() {
 }
 
 function _compcap_pretty_print() {
-    local -i max_length=0
     local -a keys=(${(k)compcap_list})
 
-    # find max length and common prefix of command
+    # find longest common prefix of command
     local common_prefix=$keys[1]
     for i ($keys) {
-        (( $#i > max_length )) && max_length=$#i
         # _find_common_prefix is slow, don't call it if they already have common prefix
         (( ${i[(i)$common_prefix]} != 1 )) && common_prefix=$(_find_common_prefix $common_prefix $i)
     }
     echo -E $common_prefix
-    max_length+=3
 
     local dsuf
     for k v (${(kv)compcap_list}) {
@@ -110,18 +105,14 @@ function _compcap_pretty_print() {
                 dsuf=/
             }
         }
-        if [[ -z $v[dscr] ]] {
-            echo -E $k$'\0'$dsuf
-        } else {
-            printf "%-${max_length}s${v[dscr]}\n" $k$'\0'
-        }
+        echo -E $k$'\0'$dsuf
     }
 }
 
 # TODO: can I use `compadd` to apply my choice?
 function fuzzy-complete() {
     local -A compcap_list
-    local selected
+    local choice
 
     IN_FZF_TAB=1
     zle expand-or-complete
@@ -130,13 +121,13 @@ function fuzzy-complete() {
     if (( $#compcap_list == 0 )) {
         return
     } elif (( $#compcap_list == 1 )) {
-        selected=$(_compcap_pretty_print | _fuzzy_select first)
+        choice=$(_compcap_pretty_print | _fuzzy_select first)
     } else {
-        selected=$(_compcap_pretty_print | sort | _fuzzy_select)
+        choice=$(_compcap_pretty_print | sort | _fuzzy_select)
     }
 
-    if [[ -n $selected ]] {
-        local -A v=("${(@0)${compcap_list[$selected]}}")
+    if [[ -n $choice ]] {
+        local -A v=("${(@0)${compcap_list[$choice]}}")
         # if RBUFFER doesn't starts with SUFFIX, the completion position is at LBUFFER
         if (( $RBUFFER[(i)$v[SUFFIX]] != 1 )) {
             LBUFFER=${LBUFFER/%$v[SUFFIX]}
@@ -144,12 +135,12 @@ function fuzzy-complete() {
             RBUFFER=${RBUFFER/#$v[SUFFIX]}
         }
         # don't add slash if have hsuf, so that /u/l/b can be expanded to /usr/lib/b not /usr/lib//b
-        if [[ -z $v[hsuf] && -d ${~${v[hpre]}}$selected ]] {
-            selected+=/
+        if [[ -z $v[hsuf] && -d ${~${v[hpre]}}$choice ]] {
+            v[word]+=/
         }
-        LBUFFER=${LBUFFER/%$v[PREFIX]}$v[ipre]$v[apre]$v[hpre]$selected$v[hsuf]$v[asuf]$v[isuf]
+        LBUFFER=${LBUFFER/%$v[PREFIX]}$v[ipre]$v[apre]$v[hpre]$v[word]$v[hsuf]$v[asuf]$v[isuf]
     }
-    zle reset-prompt
+    zle redisplay
 }
 
 zle -N fuzzy-complete

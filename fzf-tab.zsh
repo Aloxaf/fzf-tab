54644d753ceb16f816c94dfb0fa8d97924d6d0bd
Aloxaf <aloxafx@gmail.com>
2020-01-08 19:58:29 +0800

    Use bash compatible syntax
    I love braces, but no editors likes it

--- fzf-tab.zsh
+++ fzf-tab.zsh
@@ -8,57 +8,56 @@
 zmodload zsh/zutil
 
 # thanks Valodim/zsh-capture-completion
-function compadd() {
+compadd() {
     emulate -L zsh
     # parse all options
     local -A apre hpre dscrs _oad
     local -a isfile _opts __
     zparseopts -E -a _opts P:=apre p:=hpre d:=dscrs O:=_oad A:=_oad D:=_oad f=isfile \
-        i: S: s: I: X: x: r: R: W: F: M+: E: q e Q n U C \
-        J:=__ V:=__ a=__ l=__ k=__ o=__ 1=__ 2=__
+               i: S: s: I: X: x: r: R: W: F: M+: E: q e Q n U C \
+               J:=__ V:=__ a=__ l=__ k=__ o=__ 1=__ 2=__
 
     # just delegate and leave if any of -O, -A or -D are given or fzf-tab is not enabled
-    if (( $#_oad != 0 || ! IN_FZF_TAB )) {
+    if (( $#_oad != 0 || ! IN_FZF_TAB )); then
         builtin compadd "$@"
         return
-    }
+    fi
 
     # store matches in $__hits and descriptions in $__dscr
     local -a __hits __dscr
-    if (( $#dscrs == 1 )) {
+    if (( $#dscrs == 1 )); then
         __dscr=( "${(@P)${(v)dscrs}}" )
-    }
+    fi
     builtin compadd -A __hits -D __dscr "$@"
-    if (( $#__hits == 0 )) {
+    if (( $#__hits == 0 )); then
         return
-    }
+    fi
 
     # store these values in compcap
     local -a keys=(apre hpre isfile PREFIX SUFFIX IPREFIX ISUFFIX)
-    local i expanded __tmp_value="<"$'\0'">" # ensure that compcap's key will always exists
-    # NOTE: I don't know why, but if I use `for i ($keys)` here I will get a coredump
-    for i ({1..$#keys}) {
-        expanded=${(P)keys[i]}
-        if [[ -n $expanded ]] {
-            __tmp_value+=$'\0'$keys[i]$'\0'$expanded
-        }
-    }
+    local key expanded __tmp_value="<"$'\0'">" # ensure that compcap's key will always exists
+    for key in $keys; do
+        expanded=${(P)key}
+        if [[ $expanded ]]; then
+            __tmp_value+=$'\0'$key$'\0'$expanded
+        fi
+    done
     _opts+=("${(@kv)apre}" "${(@kv)hpre}" $isfile)
 
     # dscr - the string to show to users
     # word - the string to be inserted
-    local dscr word
-    for i ({1..$#__hits}) {
+    local dscr word i
+    for i in {1..$#__hits}; do
         word=$__hits[i] && dscr=$__dscr[i]
-        if [[ -n $dscr ]] {
+        if [[ -n $dscr ]]; then
             dscr=${dscr//$'\n'}
-        } elif [[ -n $word ]] {
+        elif [[ -n $word ]]; then
             dscr=$word
-        } else {
+        else
             continue
-        }
+        fi
         compcap[$dscr]=$__tmp_value${word:+$'\0'"word"$'\0'$word}$'\0'"args"$'\0'${(pj:\1:)_opts}
-    }
+    done
     # tell zsh that the match is successful
     builtin compadd -Q -U ''
 }
@@ -69,16 +68,16 @@ function compadd() {
 : ${(A)=FZF_TAB_QUERY=prefix input first}
 
 # sets `query` to the valid query string
-function _fzf_tab_find_query_str() {
+_fzf_tab_find_query_str() {
     local key qtype tmp
     typeset -g query=
-    for qtype ($FZF_TAB_QUERY) {
-        if [[ $qtype == prefix ]] {
+    for qtype in $FZF_TAB_QUERY; do
+        if [[ $qtype == prefix ]]; then
             # find the longest common prefix among ${(k)compcap}
             local -a keys=(${(k)compcap})
             tmp=$keys[1]
             local MATCH match mbegin mend prefix=(${(s::)tmp})
-            for key (${keys:1}) {
+            for key in ${keys:1}; do
                 (( $#tmp )) || break
                 [[ $key == $tmp* ]] && continue
                 # interpose characters from the current common prefix and $key and see how
@@ -87,48 +86,48 @@ function _fzf_tab_find_query_str() {
                 # truncate common prefix and maintain loop invariant: ${(s::)tmp} == $prefix
                 tmp[$#MATCH/2+1,-1]=""
                 prefix[$#MATCH/2+1,-1]=()
-            }
-        } elif [[ $qtype == input ]] {
+            done
+        elif [[ $qtype == input ]]; then
             local fv=${${(v)compcap}[1]}
             local -A v=("${(@0)fv}")
             tmp=$v[PREFIX]
-            if (( $RBUFFER[(i)$v[SUFFIX]] != 1 )) {
+            if (( $RBUFFER[(i)$v[SUFFIX]] != 1 )); then
                 tmp=${tmp/%$v[SUFFIX]}
-            }
+            fi
             tmp=${${tmp#$v[hpre]}#$v[apre]}
-        }
-        if (( $FZF_TAB_QUERY[(I)longest] )) {
+        fi
+        if (( $FZF_TAB_QUERY[(I)longest] )); then
             (( $#tmp > $#query )) && query=$tmp
-        } elif [[ -n $tmp ]] {
+        elif [[ -n $tmp ]]; then
             query=$tmp && break
-        }
-    }
+        fi
+    done
 }
 
 # pupulates array `candidates` with completion candidates
-function _fzf_tab_get_candidates() {
+_fzf_tab_get_candidates() {
     local dsuf k _v filepath
     typeset -ga candidates=()
-    for k _v (${(kv)compcap}) {
+    for k _v in ${(kv)compcap}; do
         local -A v=("${(@0)_v}")
         # add a character to describe the type of the files
         # TODO: can be color?
         dsuf=
-        if [[ -n $v[isfile] ]] {
+        if [[ -n $v[isfile] ]]; then
             filepath=${(Q)~${v[hpre]}}${(Q)k}
-            if [[ -L $filepath ]] {
+            if [[ -L $filepath ]]; then
                 dsuf=@
-            } elif [[ -d $filepath ]] {
+            elif [[ -d $filepath ]]; then
                 dsuf=/
-            }
-        }
+            fi
+        fi
         candidates+=$k$'\0'$dsuf
-    }
+    done
     local LC_ALL=C
     candidates=("${(@on)candidates}")
 }
 
-function _fzf_tab_complete() {
+_fzf_tab_complete() {
     local -A compcap
     local choice
 
@@ -139,39 +138,40 @@ function _fzf_tab_complete() {
     emulate -L zsh
 
     case $#compcap in
-      0) return;;
-      1) choice=${(k)compcap};;
-      *)
-        local query candidates=()
-        _fzf_tab_find_query_str  # sets `query`
-        _fzf_tab_get_candidates  # sets `candidates`
-        choice=$($FZF_TAB_COMMAND ${(z)FZF_TAB_OPTS} ${query:+-q$query} <<<${(pj:\n:)candidates})
-        choice=${choice%%$'\0'*}
-      ;;
-    }
+        0) return;;
+        1) choice=${(k)compcap};;
+        *)
+            local query candidates=()
+            _fzf_tab_find_query_str  # sets `query`
+            _fzf_tab_get_candidates  # sets `candidates`
+            choice=$($FZF_TAB_COMMAND ${(z)FZF_TAB_OPTS} ${query:+-q$query} <<<${(pj:\n:)candidates})
+            choice=${choice%%$'\0'*}
+            ;;
+    esac
 
     compstate[insert]=
     compstate[list]=
-    if [[ -n $choice ]] {
+    if [[ -n $choice ]]; then
         local -A v=("${(@0)${compcap[$choice]}}")
         local -a args=("${(@ps:\1:)v[args]}")
-        IPREFIX=$v[IPREFIX] PREFIX=$v[PREFIX] SUFFIX=$v[SUFFIX] ISUFFIX=$v[ISUFFIX] builtin compadd "${args[@]:-Q}" -Q -- $v[word]
+        IPREFIX=$v[IPREFIX] PREFIX=$v[PREFIX] SUFFIX=$v[SUFFIX] ISUFFIX=$v[ISUFFIX] \
+               builtin compadd "${args[@]:-Q}" -Q -- $v[word]
         # the first result is '' (see the last line of compadd)
         compstate[insert]='2'
         (( ! FZF_TAB_INSERT_SPACE )) || [[ $RBUFFER == ' '* ]] || compstate[insert]+=' '
-    }
+    fi
 }
 
 zle -C _fzf_tab_complete complete-word _fzf_tab_complete
 
-function fzf-tab-complete() {
+fzf-tab-complete() {
     zle _fzf_tab_complete  # must run with user options; don't add `emulate -L zsh` above this line
     zle redisplay
 }
 
 zle -N fzf-tab-complete
 
-function disable-fzf-tab() {
+disable-fzf-tab() {
     emulate -L zsh
     if (( $+_fzf_tab_orig_widget )); then
         bindkey '^I' $_fzf_tab_orig_widget
@@ -179,7 +179,7 @@ function disable-fzf-tab() {
     fi
 }
 
-function enable-fzf-tab() {
+enable-fzf-tab() {
     emulate -L zsh
     typeset -g _fzf_tab_orig_widget="${$(bindkey '^I')##* }"
     bindkey '^I' fzf-tab-complete

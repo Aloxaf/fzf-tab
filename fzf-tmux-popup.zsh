e472b75a9add918a243e3634e8c916a499170346
Aloxaf <aloxafx@gmail.com>
2020-10-18 23:51:49 +0800

    add height and width

--- fzf-tmux-popup.zsh
+++ fzf-tmux-popup.zsh
@@ -4,7 +4,7 @@ mkfifo /tmp/fzf-tab-$$
 
 cat > /tmp/fzf-tab-list-$$
 
-tmux popup -KE -R "fzf $1 < /tmp/fzf-tab-list-$$ > /tmp/fzf-tab-$$" &
+tmux popup -w80% -h50% -KE -R "fzf $1 < /tmp/fzf-tab-list-$$ > /tmp/fzf-tab-$$" &
 
 cat /tmp/fzf-tab-$$
 

e8218676ff86fa4120a342e266180de33c968d30
Aloxaf <aloxafx@gmail.com>
2020-10-22 10:04:36 +0800

    refactor: move tmp files to /tmp/fzf-tab

--- fzf-tmux-popup.zsh
+++ fzf-tmux-popup.zsh
@@ -8,12 +8,13 @@ local -a tmp=($(tmux display-message -p "#{pane_top} #{cursor_y} #{pane_left} #{
 local cursor_y=$((tmp[1] + tmp[2])) cursor_x=$((tmp[3] + tmp[4])) window_height=$tmp[5] window_width=$tmp[6]
 
 # create fifo and write to completion lists to file
-touch /tmp/fzf-tab-$$
-cat > /tmp/fzf-tab-list-$$
+[[ -d /tmp/fzf-tab ]] || mkdir /tmp/fzf-tab
+touch /tmp/fzf-tab/result-$$
+cat > /tmp/fzf-tab/list-$$
 
 # get the size of content, note we should remove all ANSI color code
-local comp_lines=${#${(f)"$(</tmp/fzf-tab-list-$$)"}}
-local comp_length=$(sed 's/\x1b\[[0-9;]*m//g' /tmp/fzf-tab-list-$$ | awk 'length > max_length { max_length = length; } END { print max_length }')
+local comp_lines=${#${(f)"$(</tmp/fzf-tab/list-$$)"}}
+local comp_length=$(sed 's/\x1b\[[0-9;]*m//g' /tmp/fzf-tab/list-$$ | awk 'length > max_length { max_length = length; } END { print max_length }')
 
 # calculate the popup height and y position
 if (( cursor_y * 2 > window_height )); then
@@ -31,12 +32,12 @@ fi
 local popup_width=$(( comp_length + 4 > window_width ? window_width : comp_length + 4 ))
 local popup_x=$(( cursor_x + popup_width > window_width ? window_width - popup_width : cursor_x ))
 
-echo -E "fzf ${(qq)fzf_opts[@]} < /tmp/fzf-tab-list-$$ > /tmp/fzf-tab-$$"  > /tmp/fzf-tab-tmux.zsh
+echo -E "fzf ${(qq)fzf_opts[@]} < /tmp/fzf-tab/list-$$ > /tmp/fzf-tab/result-$$" > /tmp/fzf-tab/fzf-$$
 {
   tmux popup -x $popup_x -y $popup_y \
        -w $popup_width -h $popup_height \
-       -KE -R "zsh /tmp/fzf-tab-tmux.zsh"
-  cat /tmp/fzf-tab-$$
+       -KE -R "zsh -f /tmp/fzf-tab/fzf-$$"
+  cat /tmp/fzf-tab/result-$$
 } always {
-  rm /tmp/fzf-tab-$$ /tmp/fzf-tab-list-$$
+  rm /tmp/fzf-tab/*-$$
 }

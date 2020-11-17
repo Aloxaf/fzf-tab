Aloxaf/fzf-tab

commit cc3b079467a1ac9677ea38598c7a35a6037b5877

fix: Check also .bundle as a module extension

fzf-tab.zsh
@@ -319,7 +319,7 @@ typeset -ga _ftb_group_colors=(
           "See https://github.com/Aloxaf/fzf-tab/pull/132 for more information%f%b"
   fi
 
-  if [[ -e $FZF_TAB_HOME/modules/Src/aloxaf/fzftab.so ]]; then
+  if [[ -e $FZF_TAB_HOME/modules/Src/aloxaf/fzftab.(so|bundle) ]]; then
     module_path+=("$FZF_TAB_HOME/modules/Src")
     zmodload aloxaf/fzftab
 

commit b2aae4cca5b1838d406d146a6118fb4aa12228c9

fix: fix bug introduced in #159

fzf-tab.zsh
@@ -319,7 +319,7 @@ typeset -ga _ftb_group_colors=(
           "See https://github.com/Aloxaf/fzf-tab/pull/132 for more information%f%b"
   fi
 
-  if [[ -e $FZF_TAB_HOME/modules/Src/aloxaf/fzftab.(so|bundle) ]]; then
+  if [[ -n $FZF_TAB_HOME/modules/Src/aloxaf/fzftab.(so|bundle)(#qN) ]]; then
     module_path+=("$FZF_TAB_HOME/modules/Src")
     zmodload aloxaf/fzftab
 